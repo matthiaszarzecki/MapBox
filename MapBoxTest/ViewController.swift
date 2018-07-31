0ede813bd960ff4b2a90d52ef2b7ca463906afd5
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2018-07-31 16:31:47 +0200

    added basic map functionality

--- MapBoxTest/ViewController.swift
+++ MapBoxTest/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import Mapbox
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MGLMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MGLMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //set map center location
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 52.559185, longitude: 13.365666), zoomLevel: 9, animated: false)
+        
+        //set map style
+        mapView.styleURL = MGLStyle.lightStyleURL
+        
+        //add a point annotation
+        let annotation = MGLPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 52.559185, longitude: 13.365666)
+        annotation.title = "Central Park"
+        annotation.subtitle = "The biggest park in New York City!"
+        mapView.addAnnotation(annotation)
+        
+        // Set the map view's delegate
+        mapView.delegate = self
+        
+        // Allow the map view to display the user's location
+        mapView.showsUserLocation = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Implement the delegate method that allows annotations to show callouts when tapped
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        // Always allow callouts to popup when annotations are tapped.
+        return true
     }
-
-
+    
+    // Zoom to the annotation when it is selected
+    func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
+        let camera = MGLMapCamera(lookingAtCenter: annotation.coordinate, fromDistance: 4000, pitch: 0, heading: 0)
+        mapView.fly(to: camera, completionHandler: nil)
+    }
 }
-
